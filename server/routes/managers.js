e4f17008ecdc3274fcb187a94c9a60657f1b78f2
lukaslgit <lukas.lutonsky.sk@gmail.com>
2025-11-29 19:07:25 +0100

    feat(manager): implement manager login

--- server/routes/managers.js
+++ server/routes/managers.js
@@ -2,7 +2,9 @@ import express from 'express'
 const router = express.Router()
 import bcrypt from 'bcrypt'
 import { pool } from '../db/db.js'
+import jwt from 'jsonwebtoken'
 
+// MANAGER REGISTER
 router.post('/register', async (req, res) => {
     const {first_name, last_name, email, password, key} = req.body
     const managerSecretKey = 123456 // TODO ADD REAL KEY TO ENV OR TO DATABASE
@@ -33,7 +35,45 @@ router.post('/register', async (req, res) => {
     } catch (error) {
         res.status(400).json({'error': 'Something went wrong, please try again later!'})
     }
+})
 
+// MANAGER LOGIN
+router.post('/login', async (req, res) => {
+    const {email, password} = req.body
+    try {
+        if(!email || !password){
+            res.status(400).json({'error': 'All fields must be filled!'})
+            return
+        }
+
+        const user = await pool.query('SELECT manager_id, first_name, email, password FROM managers WHERE email = $1', [email])
+
+        if(user.rows.length == 0){
+            res.status(400).json({'error': 'User does not exist'})
+            return
+        }
+
+        const matchPass = await bcrypt.compare(password, user.rows[0].password)
+
+        if(!matchPass){
+            res.status(400).json({'error': 'Wrong password!'})
+            return
+        }
+
+        const token = jwt.sign({'worker_id': user.rows[0].manager_id, 'name': user.rows[0].first_name, 'email': user.rows[0].email, 'role': 'manager'}, process.env.SECRET, {
+            expiresIn: '5m'
+        })
+
+        res.cookie('token', token, {
+                httpOnly: true,
+                sameSite: "strict",
+                maxAge: 5 * 60 * 1000
+            })
+
+        res.status(200).json({'message': 'Logged in!'})
+    } catch (error) {
+        res.status(400).json({'error': 'Something went wrong, please try again later!'})
+    }
 })
 
 export default router
